d1f75238b581498bed96d6cc36f7f49e94df8cc3
dracosptifire <austin3flores@gmail.com>
2022-04-14 13:56:37 -0700

    first commit

--- Array Heven/main.cpp
+++ Array Heven/main.cpp
@@ -1,14 +1,52 @@
-//
 //  main.cpp
-//  Array Heven
+//  Array-Heven
 //
-//  Created by Austin Flores on 4/14/22.
-//
+//  Created by Austin Flores on 2/22/14.
+//  Copyright (c) 2014 Austin Flores. All rights reserved.
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;       //requred to access entities with namespace std
+
+void averageNUM(int total, double NUMS[])
+{
+    double average;
+    double sum = 0;
+    
+    for (int n = 0; n < (total-1); n++)
+        sum = NUMS[n] + sum;
+    cout << sum;
+    cout << "\n\n";
+    average = sum / (total-1);
+    cout << average;
+    cout << "\n\n";
+}
+
+void printNumgreater() {
+}
+ 
+int main() {
+    int const SIZE = 100 ;
+    double nums[SIZE];
+    
+    int sentinel = 0;
+    int i = 0;
+    
+    while (sentinel != -1) {
+        cout << "enter number:\n>";
+        cin >> nums[i];
+        if (nums[i++] == -1)
+            sentinel = -1;
+    }
+    
+    cout << "\n\n";
+    
+    averageNUM(i, nums);
+    
+    for ( int n = 0; n < (i - 1); n++)
+        cout << nums[n] << " ";
+    
+    cout << "\n\n\t";
     return 0;
 }
